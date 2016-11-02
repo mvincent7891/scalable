d3ca0304ff37fcaacf5f4fdee027eeb9774d6753
Michael Parlato <michaelvparlato@gmail.com>
2016-11-02 14:21:03 -0700

    skeleton commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/main.js",
+  output: {
+    path: "./",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
